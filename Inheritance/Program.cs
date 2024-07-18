d5ad0d99af83cea2aff1a71fb21fce7a6dadfa7d
Dan Marino <technicaldanny302@gmail.com>
2024-07-18 16:03:26 -0700

    Completed Exercise

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,13 +6,13 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
+            // DONE Be sure to follow best practice when creating your classes
 
-            // Create a class Animal
+            // DONE a class Animal
             // give this class 4 members that all Animals have in common
 
 
-            // Create a class Bird
+            // DONE a class Bird
             // give this class 4 members that are specific to Bird
             // Set this class to inherit from your Animal Class
 
@@ -29,6 +29,31 @@ namespace Inheritance
              * Creatively display the class member values 
              */
 
+            var myBird = new Bird();
+            myBird.WingColor = "blue";
+            myBird.CanFly = true;
+            myBird.CanFly = true;
+            myBird.beakLength = 3.5;
+
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "Swamp",
+                CanGrowTail = true
+            };
+
+            var myAnimal = new Animal[] { myBird, lizard };
+
+            foreach (var animal in myAnimal)
+            {
+                Console.WriteLine($"Alive:{animal.IsAlive}");
+                Console.WriteLine($"Age: {animal.Age} Years Old");
+                Console.WriteLine($"It Has {animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+                            
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Reptile class
              *  
